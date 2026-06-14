fa68d99a5a7803a2c2ee7e8e7bff8935e686ca3a
Jonathan Martin <j.m.desloges@gmail.com>
2026-06-14 15:18:49 -0400

    Introducing circle as Sprite

--- main.py
+++ main.py
@@ -9,13 +9,19 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
     while True:
         log_state()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill("black")
         pygame.display.flip()
+        # Limit framerate to 60 fps
+        dt = clock.tick(60) / 1000
 
 
 
